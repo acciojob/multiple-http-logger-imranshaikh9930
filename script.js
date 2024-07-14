1fa7ed631ab04f9a3ad0cfc239bb598a8d089ca8
imran <87297004+imranshaikh9930@users.noreply.github.com>
2024-07-14 20:05:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ const urls = [
 
 // Don't change the above line
 // Write your code here
+
+const fetchData = async(url)=>{
+		try {
+			const resp = await fetch(url);
+			const data = await resp.json();
+			console.log(data);
+		} catch (error) {
+				console.log(error);
+		}
+}
+
+urls.forEach((url)=>{
+	fetchData(url);
+})
